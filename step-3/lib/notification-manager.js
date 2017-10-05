f8ef4eb3a84b494486c9ec668b7f2cfa6d96fc63
Adam Markham <1291252+mountainraptor@users.noreply.github.com>
2017-10-05 08:00:54 -0600

    Completed Step-3 System Push Notification
    * Display a push notification
    TODO: complete step-2
    TODO: update tutorial README for step-3

---
+++ step-3/lib/notification-manager.js
@@ -0,0 +1,18 @@
+/**
+
+**/
+(() => {
+  'use strict';
+
+  class NotificationManager {
+    load(messageCenter) {
+      return Promise.resolve();
+    }
+
+    unload() {
+      return Promise.resolve();
+    }
+  }
+
+  module.exports = NotificationManager;
+})();
